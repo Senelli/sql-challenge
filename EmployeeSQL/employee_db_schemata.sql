0d7cd6bb91d735d495a1840df8902eef06a7114d
Senelli <senellijinadasa@gmail.com>
2023-12-17 01:01:45 -0800

    formatted create statements

--- EmployeeSQL/employee_db_schemata.sql
+++ EmployeeSQL/employee_db_schemata.sql
@@ -25,28 +25,28 @@ CREATE TABLE employees (
     last_name VARCHAR(50) NOT NULL,
     sex VARCHAR(1) NOT NULL,
     hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
     emp_no INT NOT NULL,
     salary INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
     dept_no VARCHAR(5) NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager (
     dept_no VARCHAR(5) NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
     emp_no INT NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	PRIMARY KEY (emp_no)
 );
